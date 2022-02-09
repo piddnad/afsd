9ca3fd7ca377afcbda8e5d8fb511230a846b97d9
Piddnad <piddnad@gmail.com>
2022-02-09 23:10:26 +0800

    add fsod setting

--- run_coco.sh
+++ run_coco.sh
@@ -15,10 +15,44 @@ python3 -m tools.train_net --num-gpus 8 \
         --opts OUTPUT_DIR ${SAVEDIR}/frcn_r101_base
 
 
+# -------------------------------  FSOD (only novel 20 class) ---------------------------------- #
+# ------------------------------ Model Preparation -------------------------------- #
+python3 -m tools.ckpt_surgery \
+        --src ${SAVEDIR}/frcn_r101_base/model_final.pth \
+        --method remove \
+        --save-dir ${SAVEDIR}/frcn_r101_base \
+        --dataset coco
+BASE_WEIGHT=${SAVEDIR}/frcn_r101_base/model_reset_remove.pth
 
+
+# ------------------------------ Novel Fine-tuning -------------------------------- #
+# --> 1. FSRW-like, i.e. run seed0 10 times (the FSOD results on coco in most papers)
+for repeat_id in 0 1 2 3 4 5 6 7 8 9
+do
+    for shot in 10
+    do
+        for seed in 0
+        do
+            python3 -m tools.create_config \
+                    --dataset coco --config_root configs/coco \
+                    --shot ${shot} --seed ${seed} --setting 'fsod'
+            CONFIG_PATH=configs/coco/frcn_fsod_r101_novel_${shot}shot_seed${seed}.yaml
+            OUTPUT_DIR=${SAVEDIR}/frcn_fsod_r101_novel/fsrw-like/${shot}shot_seed${seed}_repeat${repeat_id}
+            python3 -m tools.train_net --num-gpus 8 \
+                    --config-file ${CONFIG_PATH} \
+                    --opts MODEL.WEIGHTS ${BASE_WEIGHT} OUTPUT_DIR ${OUTPUT_DIR}
+            rm ${CONFIG_PATH}
+            rm ${OUTPUT_DIR}/model_final.pth
+        done
+    done
+done
+python3 tools/extract_results.py --res-dir ${SAVEDIR}/defrcn_fsod_r101_novel/fsrw-like --shot-list 1 2 3 5 10 30  # surmarize all results
+
+
+# -------------------------------  GFSOD (80 class) ---------------------------------- #
 # ----------------------------- Model Preparation --------------------------------- #
 python3 -m tools.ckpt_surgery \
-        --src checkpoints/coco/faster_rcnn/faster_rcnn_R_101_FPN_base_2x/model_final.pth \
+        --src ${SAVEDIR}/frcn_r101_base/model_final.pth \
         --method randinit \
         --save-dir ${SAVEDIR}/frcn_r101_base \
         --dataset coco --no-bg
